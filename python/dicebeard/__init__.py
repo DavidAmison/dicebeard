85cc0632ee1625ad8347301a1089fda9e70844c7
David <david.amison20@gmail.com>
2017-02-06 12:18:11 +0000

    Improved image creation
    Put image manipulation in seperate file
    Added more images
    Improved code

--- python/dicebeard/__init__.py
+++ python/dicebeard/__init__.py
@@ -9,6 +9,8 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
+from . import img_manip
+
 import telepot
 import telepot.aio
 from skybeard.beards import BeardChatHandler
@@ -20,31 +22,7 @@ def get_args(msg_text):
     return msg_text.split(" ")[1:]
 
 
-def print_dice_img(self, sides, value):
-    '''generated an image of a dice with the printed number value'''
-    images = Path(os.path.dirname(__file__))
-    num = int(value)
-    
-    dice_img = images / 'images' / ('d'+str(sides) + '.png')
-    output_img = images / 'images' / 'output.png'
-    
-    #set x and y co-ordinates based on number
-    if num < 10:
-        x = 45
-        y = 39
-    else:
-        x = 38
-        y = 37
-               
-    #Printing of number on the dice 
-    img = Image.open(str(dice_img))
-    draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype('arial.ttf', 20)
-    draw.text((x,y),str(num),(255,255,255), font = font)
-    img.save(str(output_img))
-    
-    #return the file
-    return output_img
+
 
 
 dice_faces = {
@@ -83,12 +61,17 @@ class DiceBeard(BeardChatHandler):
         
         await self.sender.sendMessage("{}".format(text))
         
-        #Roll a d20 for good measure
-        roll = dice.roll('1d20')
-        output_img = print_dice_img(self, 20,roll)
+        #Roll another die for good measure
+        roll = dice.roll('1d6')
+        output_img = img_manip.create_dice_img(self,6,roll)
         #Post the image to the screen
         await self.sender.sendPhoto(open(str(output_img),'rb'))
         
+        #Roll another die for good measure
+        roll = dice.roll('1d10')
+        output_img = img_manip.create_dice_img(self,10,roll)
+        #Post the image to the screen
+        await self.sender.sendPhoto(open(str(output_img),'rb'))
         
 
             
--- python/dicebeard/img_manip.py
+++ python/dicebeard/img_manip.py
@@ -4,8 +4,49 @@ Created on Sat Feb  4 21:43:28 2017
 
 @author: David
 """
+from pathlib import Path
+import os
+
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+
 
 #Python code for image manipulation and production for dicebeards
+
+def create_dice_img(self, sides, value):
+    '''Generates an image of a dice with the printed number value. Returns the
+    file reference'''
+    images = Path(os.path.dirname(__file__))
+    num = int(value)
+    
+    dice_img = images / 'images' / ('d'+str(sides) + '.png')
+    output_img = images / 'images' / 'output.png'
+    
+          
+    #set the correct font size depending on dice
+    font_sz = 20
+    if int(sides) == 6:
+        font_sz = 50
+               
+    #Printing of number on the dice 
+    img = Image.open(str(dice_img))
+    
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype('arial.ttf', font_sz)
+    #set x and y co-ordinates based on number
+    W, H = img.size
+    msg = str(num)
+    w, h = draw.textsize(msg, font=font)
+    x = (W-w)/2
+    y = (H-h)/2
+    
+    draw.text((x,y),str(num),(255,255,255), font = font)
+    img.save(str(output_img))
+    
+    #return the file
+    return output_img
+
 
 
     
