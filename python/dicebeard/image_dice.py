4b7111528040243e516141704b1163214bcdc76c
Nathanael Farley <nasfarley88@gmail.com>
2017-06-12 13:09:14 +0100

    Reintroduce bug in image generation (so it can be fixed!)

--- python/dicebeard/image_dice.py
+++ python/dicebeard/image_dice.py
@@ -44,7 +44,7 @@ class Dice:
             width, height = die_img.size
             #add the image to the output image
             # TODO fix this bug 
-            # out_img.paste(die_img,[int(points[n][0]-(width/2)),int(points[n][1]-(height/2))],die_img)
+            out_img.paste(die_img,[int(points[n][0]-(width/2)),int(points[n][1]-(height/2))],die_img)
         bytes_out = io.BytesIO()
         out_img.save(bytes_out, format='PNG')
         bytes_out = bytes_out.getvalue()
