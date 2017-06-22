cce7c118e8627fdf1effcd359f769d503bc37d0f
David <david.amison20@gmail.com>
2017-06-22 13:49:14 +0100

    Some changes to random point generation

--- python/dicebeard/skb_roll/random_points.py
+++ python/dicebeard/skb_roll/random_points.py
@@ -2,6 +2,7 @@ import random
 from shapely.geometry import Polygon
 from shapely import affinity
 import math
+import numpy as np
 
 
 def random_shape_placement(n, box, shape):
@@ -40,12 +41,12 @@ def _push_to_nearest(shapes, frame):
     find a space
     '''
     for i, shape_1 in enumerate(shapes):
-        shapes_excluding_1 = [shape for k, shape in enumerate(shapes) if k != i]
+        shapes_upto_1 = shapes[:i]
         if not frame.contains(shape_1):
-            shapes[i] = _move_shape(shape_1, shapes_excluding_1, frame)
-        for shape_2 in shapes_excluding_1:
+            shapes[i] = _move_shape(shape_1, shapes_upto_1, frame)
+        for shape_2 in shapes_upto_1:
             if shape_1.intersects(shape_2):
-                shapes[i] = _move_shape(shape_1, shapes_excluding_1, frame)
+                shapes[i] = _move_shape(shape_1, shapes_upto_1, frame)
 
     return shapes
 
@@ -76,3 +77,79 @@ def _does_shape_intersect(shape_1, shapes, frame):
         return True
 
     return False
+
+
+def rand_points_with_push(n, box, sep):
+    '''
+    Generate a set of n random points within a box.
+
+    Box should be a tuple containing the minimum and maximum co-ordinates
+    desired in the form (xmin, xmax, ymin, ymax)
+    '''
+
+    # Generate the initial set of points
+    x_coord = np.random.randint(box[0], box[1], (1, n)).astype(float)
+    y_coord = np.random.randint(box[2], box[3], (1, n)).astype(float)
+
+    return _push_points(x_coord, y_coord, box, sep)
+
+
+def _push_points(x_coord, y_coord, box, sep):
+    n = len(x_coord[0])
+    force_const = sep * 2
+    found_solution = False
+    for foo in range(1, 10000000):
+        found_solution = True
+        vel_x = np.zeros((n, n))
+        vel_y = np.zeros((n, n))
+        vel_walls = np.zeros((n, 4))
+
+        one_array = np.ones((n, 1))
+        X = (one_array * x_coord)
+        Y = (one_array * y_coord)
+        dX = X.T - X
+        dY = Y.T - Y
+        # Find the force between the points
+        force_x = np.zeros((n, n))
+        force_y = np.zeros((n, n))
+        for i in range(0, n):
+            for j in range(0, n):
+                D = math.sqrt(dX[i][j]*dX[i][j] + dY[i][j]*dY[i][j])
+                if D < sep and i != j:
+                    found_solution = False
+                if i != j and D != 0:
+                    force_x[i][j] = force_const*dX[i][j]/(D*D*D)
+                    force_y[i][j] = force_const*dY[i][j]/(D*D*D)
+                elif i != j and D == 0:
+                    force_x[i][j] = force_const
+
+        # Find the new velocity (based on push from all other points)
+        vel_x = vel_x + force_x
+        vel_y = vel_y + force_y
+        # Adjust the velocity based on push from the walls
+        for i, vel_all in enumerate(vel_walls):
+            for j, vel in enumerate(vel_all):
+                coord = x_coord[0][i] if j < 2 else y_coord[0][i]
+                D = coord - box[j]
+                if abs(D) < sep:
+                    found_solution = False
+                vel_walls[i][j] += force_const*abs(D)/(D*D*D)
+
+        # Check if the current points are a solution
+        if found_solution:
+            return np.append(x_coord, y_coord, axis=0).astype(int).T.tolist()
+
+        # Move the points based on their current velocity
+        for i in range(0, n):
+            t = (box[1]+box[3]-box[0]-box[2])/15
+            x_coord[0][i] += (sum(vel_x[i])+sum(vel_walls[i][:2]))*t
+            y_coord[0][i] += (sum(vel_y[i])+sum(vel_walls[i][2:]))*t
+
+            # If the dice is out of bounds centre it on that axis
+            if x_coord[0][i] < box[0] or x_coord[0][i] > box[1]:
+                x_coord[0][i] = (box[0]+box[1])/2
+            if y_coord[0][i] < box[2] or y_coord[0][i] > box[3]:
+                y_coord[0][i] = (box[2]+box[3])/2
+
+    return np.append(x_coord, y_coord, axis=0).astype(int).T.tolist()
+
